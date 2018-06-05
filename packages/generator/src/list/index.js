05814de3beebd5dda404bf045fd5ae3827341a37
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2018-06-05 13:14:29 -0300

    feat(template): add new list template with modern support

--- packages/generator/src/list/index.js
+++ packages/generator/src/list/index.js
@@ -1,11 +1,8 @@
+// @flow
 import Generator from 'yeoman-generator';
 import pluralize from 'pluralize';
-import {
-  getConfigDir,
-  getRelativeConfigDir,
-  camelCaseText,
-  uppercaseFirstLetter,
-} from '../utils';
+import { getConfigDir } from '../config';
+import { uppercaseFirstLetter } from '../ejsHelpers';
 
 class ListGenerator extends Generator {
   constructor(args, options) {
@@ -21,34 +18,18 @@ class ListGenerator extends Generator {
     this.destinationDir = getConfigDir('list');
   }
 
-  _getConfigDirectories() {
-    return getRelativeConfigDir('loader', ['model', 'connection']);
-  }
-
   generateList() {
-    // const schema = this.options.model ?
-    //   getMongooseModelSchema(this.options.model, true)
-    //   : null;
-
     const name = uppercaseFirstLetter(this.options.name);
 
     const templatePath = this.templatePath('List.js.template');
 
-    // const templatePath = schema ?
-    //   this.templatePath('LoaderWithSchema.js.template')
-    //   : this.templatePath('Loader.js.template');
-    //
-    // const directories = this._getConfigDirectories();
-
     const pluralName = pluralize(this.options.name);
 
     const destinationPath = this.destinationPath(`${this.destinationDir}/${name}List.js`);
+
     const templateVars = {
       name,
-      rawName: this.options.name,
-      camelCaseName: camelCaseText(name),
       pluralName,
-      pluralCamelCaseName: camelCaseText(pluralName),
     };
 
     this.fs.copyTpl(templatePath, destinationPath, templateVars);
